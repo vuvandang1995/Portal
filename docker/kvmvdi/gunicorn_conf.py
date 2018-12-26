f5f50e632644a15f7c4ac9ecab975e50d07fa962
vuvandang1995 <dangdiendao@gmail.com>
2018-12-26 17:00:40 +0700

    remove docker

--- docker/kvmvdi/gunicorn_conf.py
+++
@@ -1,7 +0,0 @@
-# Gunicorn configuration file
-
-bind = '0.0.0.0:8889'
-
-loglevel = 'info'
-errorlog = '-'
-accesslog = '-'
--- docker/kvmvdi/superadmin/plugin/get_tokens.py
+++
@@ -1,9 +0,0 @@
-from keystoneauth1.identity import v3
-from keystoneauth1 import session
-
-
-def getToken(ip, username, password, project_name, user_domain_id, project_domain_id):
-    auth = v3.Password(auth_url="http://"+ip+":5000/v3", username=username, password=password,
-                       project_name=project_name, user_domain_id=user_domain_id, project_domain_id=project_domain_id)
-    sess = session.Session(auth=auth)
-    return sess.get_token(auth)
